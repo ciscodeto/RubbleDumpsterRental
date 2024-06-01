15903d0034703c3263e7818eb4c77b01a160ace9
Estevan Sousa <estevanmartins03@gmail.com>
2024-06-01 16:23:04 -0300

    refactor: key of ClientDAO changed to cpf

--- src/main/java/com/view/rubbledumpsterrental/model/useCases/client/InsertClientUseCase.java
+++ src/main/java/com/view/rubbledumpsterrental/model/useCases/client/InsertClientUseCase.java
@@ -1,4 +1,38 @@
 package com.view.rubbledumpsterrental.model.useCases.client;
 
+import com.view.rubbledumpsterrental.model.Notification;
+import com.view.rubbledumpsterrental.model.Validator;
+import com.view.rubbledumpsterrental.model.entities.Client;
+import com.view.rubbledumpsterrental.model.entities.RubbleDumpster;
+import com.view.rubbledumpsterrental.model.entities.RubbleDumpsterStatus;
+import com.view.rubbledumpsterrental.model.entities.valueObjects.Cpf;
+import com.view.rubbledumpsterrental.model.useCases.rubbleDumpster.RubbleDumbsterInsertValidator;
+import com.view.rubbledumpsterrental.persistence.dao.ClientDAO;
+import com.view.rubbledumpsterrental.persistence.dao.RubbleDumpsterDAO;
+import com.view.rubbledumpsterrental.persistence.utils.EntityAlreadyExistsException;
+
 public class InsertClientUseCase {
+
+    private ClientDAO clientDAO;
+
+    public InsertClientUseCase(ClientDAO clientDAO) {
+        this.clientDAO = clientDAO;
+    }
+
+    public Integer insert(Client client) {
+
+        Validator<Client> validator = new ClientInsertValidator();
+        Notification notification = validator.validate(client);
+
+        if (notification.hasErrors())
+            throw new IllegalArgumentException(notification.errorMessage());
+
+        Cpf cpf = client.getCpf();
+        if (clientDAO.findOne(cpf).isPresent())
+            throw new EntityAlreadyExistsException("This entity already exists");
+
+        client.setStatus(RubbleDumpsterStatus.AVAILABLE);
+
+        return rubbleDumbsterDAO.create(client);
+    }
 }
--- src/main/java/com/view/rubbledumpsterrental/persistence/dao/ClientDAO.java
+++ src/main/java/com/view/rubbledumpsterrental/persistence/dao/ClientDAO.java
@@ -2,11 +2,12 @@ package com.view.rubbledumpsterrental.persistence.dao;
 
 import com.view.rubbledumpsterrental.model.entities.Client;
 import com.view.rubbledumpsterrental.model.entities.Rental;
+import com.view.rubbledumpsterrental.model.entities.valueObjects.Cpf;
 import com.view.rubbledumpsterrental.persistence.utils.DAO;
 
 import java.util.Optional;
 
-public interface ClientDAO extends DAO<Client, Integer> {
-    Optional<Client> findById(Integer id);
+public interface ClientDAO extends DAO<Client, Cpf> {
+    Optional<Client> findByCpf(Cpf cpf);
 
 }
