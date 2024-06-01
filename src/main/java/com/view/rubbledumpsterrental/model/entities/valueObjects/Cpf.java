e96bc6439503de3dc04aa4c262640e6220c056ec
Estevan Sousa <estevanmartins03@gmail.com>
2024-06-01 15:46:49 -0300

    :zap:feat: CPF isValid method implemented

--- src/main/java/com/view/rubbledumpsterrental/model/entities/valueObjects/Cpf.java
+++ src/main/java/com/view/rubbledumpsterrental/model/entities/valueObjects/Cpf.java
@@ -7,5 +7,46 @@ public class Cpf {
         this.cpf = cpf;
     }
 
-    public void isValid() {}
+    public boolean isValid() {
+        if (cpf == null || cpf.isEmpty()) {
+            return false;
+        }
+
+        // Remove pontos e traços caso existam
+        String cpfNumeros = cpf.replaceAll("\\D", "");
+
+        // O CPF deve conter 11 dígitos
+        if (cpfNumeros.length() != 11) {
+            return false;
+        }
+
+        // Verifica se todos os dígitos são iguais (caso contrário inválido)
+        if (cpfNumeros.matches("(\\d)\\1{10}")) {
+            return false;
+        }
+
+        // Calcula o primeiro dígito verificador
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
+            soma += (cpfNumeros.charAt(i) - '0') * (10 - i);
+        }
+        int primeiroDigitoVerificador = 11 - (soma % 11);
+        if (primeiroDigitoVerificador >= 10) {
+            primeiroDigitoVerificador = 0;
+        }
+
+        // Calcula o segundo dígito verificador
+        soma = 0;
+        for (int i = 0; i < 10; i++) {
+            soma += (cpfNumeros.charAt(i) - '0') * (11 - i);
+        }
+        int segundoDigitoVerificador = 11 - (soma % 11);
+        if (segundoDigitoVerificador >= 10) {
+            segundoDigitoVerificador = 0;
+        }
+
+        // Verifica se os dígitos verificadores estão corretos
+        return cpfNumeros.charAt(9) - '0' == primeiroDigitoVerificador &&
+                cpfNumeros.charAt(10) - '0' == segundoDigitoVerificador;
+    }
 }
---
+++ src/main/java/com/view/rubbledumpsterrental/model/useCases/client/ClientInsertValidator.java
@@ -0,0 +1,26 @@
+package com.view.rubbledumpsterrental.model.useCases.client;
+
+import com.view.rubbledumpsterrental.model.Notification;
+import com.view.rubbledumpsterrental.model.Validator;
+import com.view.rubbledumpsterrental.model.entities.Client;
+
+public class ClientInsertValidator extends Validator<Client>{
+
+    @Override
+    public Notification validate(Client client) {
+        Notification notification = new Notification();
+
+        if (client == null)
+            notification.addError("Client is null");
+
+        if (nullOrEmpty(client.getName()))
+            notification.addError("Client name is null or empty");
+
+        if (!client.getCpf().isValid())
+            notification.addError("Cpf invalido");
+
+
+        return notification;
+    }
+
+}
