978dd9f6118818127b7dfab6587b6e3c344b7f7b
Estevan Sousa <estevanmartins03@gmail.com>
2024-06-01 16:54:44 -0300

    :zap: feat: FindClientUseCase done

--- src/main/java/com/view/rubbledumpsterrental/model/useCases/client/FindClientUseCase.java
+++ src/main/java/com/view/rubbledumpsterrental/model/useCases/client/FindClientUseCase.java
@@ -1,14 +1,36 @@
 package com.view.rubbledumpsterrental.model.useCases.client;
 
+import com.view.rubbledumpsterrental.model.Validator;
 import com.view.rubbledumpsterrental.model.entities.Client;
 import com.view.rubbledumpsterrental.model.entities.RubbleDumpster;
+import com.view.rubbledumpsterrental.model.entities.valueObjects.Cpf;
+import com.view.rubbledumpsterrental.persistence.dao.ClientDAO;
 import com.view.rubbledumpsterrental.persistence.utils.EntityAlreadyExistsException;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public class FindClientUseCase {
 
-    public Optional<Client> findOne(Integer id) {
-        return null;
+    private ClientDAO clientDAO;
+
+    public FindClientUseCase(ClientDAO clientDAO) {
+        this.clientDAO = clientDAO;
     }
+
+    public Optional<Client> findOne(Integer id) {
+        if (Validator.nullOrEmpty(id))
+            throw new IllegalArgumentException("Id cannot be null or empty");
+        return clientDAO.findOne(id);
+    }
+
+    public Optional<Client> findClientByCpf(Cpf cpf) {
+        if (Validator.nullOrEmpty((Collection) cpf))
+            throw new IllegalArgumentException("CPF cannot be null or empty");
+        return clientDAO.findByCpf(cpf);
+    }
+
+    public List<Client> findAll() { return clientDAO.findAll(); }
+
 }
