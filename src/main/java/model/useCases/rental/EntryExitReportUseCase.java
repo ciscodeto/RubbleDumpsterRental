dbd21c7e87e2772b55330820161d35ee56de1d18
ciscodeto <netofcisco@gmail.com>
2024-06-02 23:00:00 -0300

    Feat: Report and Export UC initialized

---
+++ src/main/java/model/useCases/rental/EntryExitReportUseCase.java
@@ -0,0 +1,10 @@
+package model.useCases.rental;
+
+import model.entities.Rental;
+import persistence.dao.RentalDAO;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class EntryExitReportUseCase {
+}
