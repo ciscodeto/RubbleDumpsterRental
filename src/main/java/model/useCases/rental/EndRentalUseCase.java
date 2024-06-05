39820794fddf4f041ab1bb733c5cd36acf5099b5
ciscodeto <netofcisco@gmail.com>
2024-06-05 07:31:57 -0300

    Fix:EndRentalUseCase

--- src/main/java/model/useCases/rental/EndRentalUseCase.java
+++ src/main/java/model/useCases/rental/EndRentalUseCase.java
@@ -44,12 +44,15 @@ public class EndRentalUseCase {
                 .orElseThrow(() -> new EntityNotFoundException("Cannot find Rental for ID " + rentalId));
 
         rental.setEndDate(LocalDate.now());
-        rental.setRentalStatus(RentalStatus.CLOSED);
-        rentalDAO.update(rental);
 
         Integer serialNumber = rental.getRubbleDumpster().getSerialNumber();
         RubbleDumpster rubbleDumpster = findRubbleDumpsterUseCase.findOne(serialNumber).get();
+
+        rental.setRentalStatus(RentalStatus.CLOSED);
         rubbleDumpster.setStatus(RubbleDumpsterStatus.AVAILABLE);
+        rental.setFinalAmount(rental.calculateFinalAmount());
+
+        rentalDAO.update(rental);
         rubbleDumpsterDAO.update(rubbleDumpster);
     }
 }
