38737f130237a818e6c69bc9f3b501ad387f8f56
Estevan Sousa <estevanmartins03@gmail.com>
2024-06-23 15:55:18 -0300

    feat: layout of screen client finished

--- src/main/java/com/dumpRents/controller/ClientUIController.java
+++ src/main/java/com/dumpRents/controller/ClientUIController.java
@@ -1,4 +1,16 @@
 package com.dumpRents.controller;
 
+import com.dumpRents.model.entities.Client;
+import javafx.event.ActionEvent;
+
+import java.awt.*;
+
 public class ClientUIController {
+
+
+    public void saveOrUpdate(ActionEvent actionEvent) {
+    }
+
+    public void backToPreviousScene(ActionEvent actionEvent) {
+    }
 }
