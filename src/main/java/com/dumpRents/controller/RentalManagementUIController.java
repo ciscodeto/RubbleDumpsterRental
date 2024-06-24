8a45f2fd05cbeeed7cd2e3769c4d951c296be0af
ciscodeto <netofcisco@gmail.com>
2024-06-24 14:39:04 -0300

    feat: rentalUI designed

--- src/main/java/com/dumpRents/controller/RentalManagementUIController.java
+++ src/main/java/com/dumpRents/controller/RentalManagementUIController.java
@@ -1,4 +1,26 @@
 package com.dumpRents.controller;
 
+import javafx.event.ActionEvent;
+
 public class RentalManagementUIController {
+    public void findByClient(ActionEvent actionEvent) {
+    }
+
+    public void findByPeriod(ActionEvent actionEvent) {
+    }
+
+    public void endRental(ActionEvent actionEvent) {
+    }
+
+    public void rentRubbleDumpster(ActionEvent actionEvent) {
+    }
+
+    public void goToRubbleDumpsterManagement(ActionEvent actionEvent) {
+    }
+
+    public void goToClientManagement(ActionEvent actionEvent) {
+    }
+
+    public void goToReportUI(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/com/dumpRents/controller/RubbleDumpsterUIController.java
+++ src/main/java/com/dumpRents/controller/RubbleDumpsterUIController.java
@@ -3,9 +3,4 @@ package com.dumpRents.controller;
 import javafx.event.ActionEvent;
 
 public class RubbleDumpsterUIController {
-    public void backToPreviousScene(ActionEvent actionEvent) {
-    }
-
-    public void saveOrUpdate(ActionEvent actionEvent) {
-    }
 }
