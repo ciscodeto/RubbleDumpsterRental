44957b4d7d720bd3753c7a71ae6c7043b970c707
Estevan Sousa <estevanmartins03@gmail.com>
2024-06-24 11:05:02 -0300

    refactor: add methods in rubbleDumpsterManagement and rubbleDumpsterUI

--- src/main/java/com/dumpRents/controller/RubbleDumpsterManagementUIController.java
+++ src/main/java/com/dumpRents/controller/RubbleDumpsterManagementUIController.java
@@ -1,4 +1,17 @@
 package com.dumpRents.controller;
 
+import javafx.event.ActionEvent;
+
 public class RubbleDumpsterManagementUIController {
+    public void inactivateRubbleDumpster(ActionEvent actionEvent) {
+    }
+
+    public void insertRubbleDumpster(ActionEvent actionEvent) {
+    }
+
+    public void updateRubbleDumbster(ActionEvent actionEvent) {
+    }
+
+    public void comeBack(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/com/dumpRents/controller/RubbleDumpsterUIController.java
+++ src/main/java/com/dumpRents/controller/RubbleDumpsterUIController.java
@@ -1,4 +1,11 @@
 package com.dumpRents.controller;
 
+import javafx.event.ActionEvent;
+
 public class RubbleDumpsterUIController {
+    public void saveOrUpdate(ActionEvent actionEvent) {
+    }
+
+    public void backToPreviousScene(ActionEvent actionEvent) {
+    }
 }
