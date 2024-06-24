5eaec7bd046b5c284c86cb4548bd4bca7aeab1a2
ciscodeto <netofcisco@gmail.com>
2024-06-24 10:37:01 -0300

    feat: RentalController initialized

--- src/main/java/com/dumpRents/controller/RubbleDumpsterManagementUIController.java
+++ src/main/java/com/dumpRents/controller/RubbleDumpsterManagementUIController.java
@@ -1,4 +1,26 @@
 package com.dumpRents.controller;
 
+import javafx.event.ActionEvent;
+
 public class RubbleDumpsterManagementUIController {
+    public void findByClient(ActionEvent actionEvent) {
+    }
+
+    public void findByPeriod(ActionEvent actionEvent) {
+    }
+
+    public void endRental(ActionEvent actionEvent) {
+    }
+
+    public void rentRubbleDumpster(ActionEvent actionEvent) {
+    }
+
+    public void goToRubbleDumpsterManagement(ActionEvent actionEvent) {
+    }
+
+    public void goToClientManagement(ActionEvent actionEvent) {
+    }
+
+    public void goToReportUI(ActionEvent actionEvent) {
+    }
 }
