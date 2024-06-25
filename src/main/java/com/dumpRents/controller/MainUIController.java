835d64e6bf78f6912854a3ab884280c57784b4a2
ciscodeto <netofcisco@gmail.com>
2024-06-24 22:50:11 -0300

    feat: windowLoader

--- src/main/java/com/dumpRents/controller/MainUIController.java
+++ src/main/java/com/dumpRents/controller/MainUIController.java
@@ -1,13 +1,56 @@
 package com.dumpRents.controller;
 
+import com.dumpRents.model.entities.Client;
 import com.dumpRents.model.entities.Rental;
+import com.dumpRents.model.entities.RentalStatus;
+import com.dumpRents.model.entities.RubbleDumpster;
+import com.dumpRents.view.WindowLoader;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+
 
 public class MainUIController {
+    @FXML private TableView<Rental> tableView;
+    @FXML private TableColumn<Rental, String> cRentalStatus;
+    @FXML private TableColumn<Rental, String> cInitialDate;
+    @FXML private TableColumn<Rental, String> cClient;
+    @FXML private TableColumn<Rental, String> cRubbleDumpsterSN;
+    @FXML private TableColumn<Rental, String> cAddress;
+
+    private ObservableList<Rental> tableData;
+    private Client client;
+    private RubbleDumpster rubbleDumpster;
+
+    private final String WITHDRAWAL_ORDER_TEXT = "Ordem De Retirada";
+    private final String END_RENTAL_TEXT = "Finalizar Locação";
+
     @FXML
-    private TableView<Rental> tableView;
+    private void initialize() {
+        bindTableViewToItemsList();
+        bindColumnsToValueSources();
+        loadDataAndShow();
+    }
+
+    private void bindTableViewToItemsList() {
+        tableData = FXCollections.observableArrayList();
+        tableView.setItems(tableData);
+    }
+
+    public void goToRubbleDumpsterManagement(ActionEvent actionEvent) {
+        WindowLoader.setRoot();
+    }
+
+    public void goToClientManagement(ActionEvent actionEvent) {
+    }
+
+    private void bindColumnsToValueSources() {
+    }
+
+    private void loadDataAndShow() {
+    }
 
     public void findByClient(ActionEvent actionEvent) {
     }
@@ -21,11 +64,6 @@ public class MainUIController {
     public void rentRubbleDumpster(ActionEvent actionEvent) {
     }
 
-    public void goToRubbleDumpsterManagement(ActionEvent actionEvent) {
-    }
-
-    public void goToClientManagement(ActionEvent actionEvent) {
-    }
 
     public void goToReportUI(ActionEvent actionEvent) {
     }
